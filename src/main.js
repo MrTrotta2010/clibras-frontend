529c76d59a07392fce49795653595e8d776caf78
Tiago Trotta <mrtrotta2010@gmail.com>
2022-03-25 14:29:33 -0300

    Deletando tudo

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import 'spectre.css/dist/spectre.min.css'
-import 'spectre.css/dist/spectre-icons.min.css'
-
-createApp(App).mount('#app')
